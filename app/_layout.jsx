f2c1dc9ffcf6fec9dc6774c196b5d748e04d16e0
antoinehage612 <hypercoffin612@gmail.com>
2024-04-16 16:30:23 +0300

    configured appwrite, added funtionalities to sign in and sign up + added the context Provider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
+import GlobalProvider from "../context/GlobalProvider";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -25,11 +26,14 @@ const RootLayout = () => {
   if (!fontsLoaded && !error) return null;
 
   return (
-    <>
+    <GlobalProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
       </Stack>
-    </>
+    </GlobalProvider>
   );
 };
 
