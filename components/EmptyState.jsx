b162e5d4d6f1253b6b42ce8e06d390e56f34f227
antoinehage612 <hypercoffin612@gmail.com>
2024-04-17 23:26:53 +0300

    home page

---
+++ components/EmptyState.jsx
@@ -0,0 +1,29 @@
+import { router } from "expo-router";
+import { images } from "../constants";
+import CustomButton from "./CustomButton";
+import { View, Text, Image } from "react-native";
+
+const EmptyState = ({ title, subtitle }) => {
+  return (
+    <View className="flex justify-center items-center px-4">
+      <Image
+        source={images.empty}
+        resizeMode="contain"
+        className="w-[270px] h-[216px]"
+      />
+
+      <Text className="text-xl text-center font-psemibold text-white mt-2">
+        {title}
+      </Text>
+      <Text className="text-sm font-pmedium text-gray-100">{subtitle}</Text>
+
+      <CustomButton
+        title="Back to Explore"
+        handlePress={() => router.push("/home")}
+        containerStyles="w-full my-5"
+      />
+    </View>
+  );
+};
+
+export default EmptyState;
